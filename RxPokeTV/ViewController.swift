3ff5873c141ba058781e3eeccae87b1fd135ddc8
Erik LaManna <erik.lamanna@willowtreeapps.com>
2016-06-22 12:21:20 -0400

    Initial Commit

---
+++ RxPokeTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxPokeTV
+//
+//  Created by Erik LaManna on 6/22/16.
+//  Copyright © 2016 WillowTree, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
